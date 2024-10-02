0632dfb0d9bb9b4001aceea1740eaba366f40671
pooja <pooja.choudhary.che21@itbhu.ac.in>
2024-10-02 15:44:09 +0530

    Initial commit

---
+++ src/components/UserIcon/UserIcon.jsx
@@ -0,0 +1,15 @@
+import './UserIcon.css';
+import { FaCircle } from 'react-icons/fa6';
+
+const UserIcon = ({ intials, available, bgColor }) => {
+    return (
+        <div className='user' style={{ position: 'absolute', top: '10px', right: '10px' }}>
+            <div className='user_icon' style={{ backgroundColor: bgColor }}>{intials}</div>
+            <div className='dot' style={available ? { color: "#50B053" } : {}}>
+                <FaCircle />
+            </div>
+        </div>
+    );
+}
+
+export default UserIcon;
